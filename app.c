4acdaa1682c5ddf5c4d1f423e7d9d7a5e94455e8
mat1jaczyyy <dominik.matijaca@skole.hr>
2018-06-08 23:57:03 +0200

    Initial commit

---
+++ app.c
@@ -0,0 +1,200 @@
+/******************************************************************************
+ 
+ Copyright (c) 2015, Focusrite Audio Engineering Ltd.
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ * Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ 
+ * Neither the name of Focusrite Audio Engineering Ltd., nor the names of its
+ contributors may be used to endorse or promote products derived from
+ this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+    Performance-optimized Launchpad Pro Firmware modification by mat1jaczyyy
+ 
+ ******************************************************************************/
+
+#include "app.h"
+typedef enum {false, true} bool;
+
+void print_byte(u8 b) {
+	for (u8 i = 0; i < 8; i++) {
+		u8 p = 1;
+		for (u8 j = 0; j < i; j++) {
+			p *= 2;
+		}
+		
+		u8 x = b & p;
+		x >>= i;
+		x *= 63;
+		hal_plot_led(TYPEPAD, 8-i, x, x, x);
+	}
+}
+
+/*----------------------------*/
+
+u8 novation[3][128] = {
+	{0, 7, 31, 63, 63, 63, 21, 6, 63, 63, 21, 9, 63, 63, 21, 6, 33, 20, 7, 4, 18, 0, 0, 0, 18, 0, 0, 0, 18, 0, 0, 0, 18, 0, 0, 0, 18, 0, 0, 0, 18, 0, 0, 0, 18, 0, 0, 0, 33, 20, 6, 3, 63, 63, 21, 6, 63, 63, 21, 8, 63, 37, 29, 16, 0, 0, 0, 0, 0, 9, 31, 7, 63, 46, 43, 24, 3, 0, 0, 0, 15, 30, 43, 15, 63, 33, 28, 0, 14, 21, 13, 22, 12, 33, 52, 63, 63, 45, 35, 32, 14, 4, 3, 5, 5, 25, 41, 54, 53, 63, 39, 25, 7, 54, 31, 38, 35, 15, 28, 55, 39, 13, 6, 1, 45, 15, 44, 18},
+	{0, 7, 31, 63, 18, 0, 0, 0, 46, 20, 7, 6, 63, 63, 21, 6, 63, 63, 21, 10, 63, 63, 21, 6, 63, 63, 21, 6, 63, 63, 21, 7, 63, 63, 21, 6, 48, 41, 16, 3, 33, 21, 7, 1, 18, 0, 0, 0, 18, 0, 0, 0, 18, 0, 0, 0, 18, 0, 0, 0, 5, 13, 20, 24, 14, 21, 20, 0, 17, 0, 31, 7, 0, 63, 58, 63, 34, 63, 41, 10, 0, 0, 6, 8, 18, 55, 63, 63, 63, 63, 63, 34, 20, 31, 7, 0, 31, 43, 63, 22, 10, 18, 19, 5, 7, 14, 0, 20, 26, 55, 55, 44, 7, 63, 63, 37, 25, 15, 28, 63, 0, 0, 51, 16, 43, 12, 23, 5},
+	{0, 7, 31, 63, 18, 0, 0, 0, 26, 0, 0, 0, 18, 0, 0, 0, 18, 0, 0, 0, 18, 0, 0, 0, 23, 6, 3, 0, 33, 21, 7, 4, 45, 37, 13, 4, 63, 63, 20, 6, 63, 63, 21, 6, 63, 63, 21, 6, 63, 63, 24, 11, 63, 63, 21, 6, 33, 20, 7, 4, 0, 0, 0, 0, 0, 13, 31, 63, 19, 50, 31, 7, 0, 11, 1, 2, 0, 33, 63, 63, 63, 63, 30, 0, 0, 1, 5, 0, 9, 27, 50, 63, 48, 57, 63, 22, 0, 0, 0, 1, 0, 3, 13, 10, 22, 6, 2, 15, 6, 9, 11, 3, 11, 26, 46, 63, 63, 15, 28, 63, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+u8 mat1[3][128] = {
+	{0, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 11, 23, 35, 51, 47, 7, 15, 23, 39, 31, 3, 7, 11, 27, 15, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 3, 7, 11, 27, 15, 7, 15, 23, 39, 31, 11, 23, 35, 51, 47, 15, 31, 47, 62, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 9, 18, 27, 36, 45, 54, 63},
+	{0, 0, 0, 0, 15, 0, 3, 7, 11, 27, 15, 7, 15, 23, 39, 31, 11, 23, 35, 51, 47, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 62, 63, 11, 23, 35, 51, 47, 7, 15, 23, 39, 31, 3, 7, 11, 27, 15, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 9, 18, 27, 36, 45, 54, 63},
+	{0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 3, 7, 11, 27, 15, 7, 15, 23, 39, 31, 11, 23, 35, 51, 47, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 63, 63, 15, 31, 47, 62, 63, 11, 23, 35, 51, 47, 7, 15, 23, 39, 31, 3, 7, 11, 27, 15, 9, 18, 27, 36, 45, 54, 63}
+};
+u8 palette[3][128];
+u8 editing = 0;
+
+void initPalette(u8 *set) {
+	memcpy(palette, set, 384);
+}
+
+void savePalette() {
+	hal_write_flash(0, palette, 384);
+}
+
+void loadPalette() {
+	hal_read_flash(0, palette, 384);
+}
+
+void edit(u8 v, u8 c) {
+	if (v == 0) {
+		editing = c % 3;
+	} else {
+		palette[editing][v] = c % 64;
+	}
+}
+
+void led(u8 p, u8 v) {
+	if (p == 99) {
+    	hal_plot_led(TYPESETUP, 0, palette[0][v], palette[1][v], palette[2][v]);
+    } else {
+    	hal_plot_led(TYPEPAD, p, palette[0][v], palette[1][v], palette[2][v]);
+    }
+}
+
+void clear() {
+	for (int i = 1; i < 99; i++) {
+		hal_plot_led(TYPEPAD, i, 0, 0, 0);
+	}
+	hal_plot_led(TYPESETUP, 0, 0, 0, 0);
+}
+
+/*----------------------------*/
+
+bool setup = false;
+
+u8 rainbow[48][3] = {{63, 0, 0}, {63, 7, 0}, {63, 15, 0}, {63, 23, 0}, {63, 31, 0}, {63, 39, 0}, {63, 47, 0}, {63, 55, 0}, {63, 63, 0}, {55, 63, 0}, {47, 63, 0}, {39, 63, 0}, {31, 63, 0}, {23, 63, 0}, {15, 63, 0}, {7, 63, 0}, {0, 63, 0}, {0, 63, 7}, {0, 63, 15}, {0, 63, 23}, {0, 63, 31}, {0, 63, 39}, {0, 63, 47}, {0, 63, 55}, {0, 63, 63}, {0, 55, 63}, {0, 47, 63}, {0, 39, 63}, {0, 31, 63}, {0, 23, 63}, {0, 15, 63}, {0, 7, 63}, {0, 0, 63}, {7, 0, 63}, {15, 0, 63}, {23, 0, 63}, {31, 0, 63}, {39, 0, 63}, {47, 0, 63}, {55, 0, 63}, {63, 0, 63}, {63, 0, 55}, {63, 0, 47}, {63, 0, 39}, {63, 0, 31}, {63, 0, 23}, {63, 0, 15}, {63, 0, 7}};
+
+void app_timer_event() {
+	static u8 ms = 33;
+	static u8 r = 0;
+	
+	if (setup) {
+		if (++ms >= 33) {
+			ms = 0;
+			hal_plot_led(TYPESETUP, 0, rainbow[r][0], rainbow[r][1], rainbow[r][2]);
+			r = (r + 1) % 48;
+		}
+	}
+}
+
+void enterSetup() {
+	hal_plot_led(TYPEPAD, 15, 7, 0, 0); // Load Novation palette
+	hal_plot_led(TYPEPAD, 16, 7, 3, 0); // Load mat1 palette
+	hal_plot_led(TYPEPAD, 17, 0, 7, 0); // Load palette from flash
+	hal_plot_led(TYPEPAD, 18, 0, 3, 7); // Save palette to flash
+}
+
+void exitSetup() {}
+
+/*----------------------------*/
+
+u8 xy_dr[128] = {0, 116, 117, 118, 119, 120, 121, 122, 123, 0, 115, 36, 37, 38, 39, 68, 69, 70, 71, 107, 114, 40, 41, 42, 43, 72, 73, 74, 75, 106, 113, 44, 45, 46, 47, 76, 77, 78, 79, 105, 112, 48, 49, 50, 51, 80, 81, 82, 83, 104, 111, 52, 53, 54, 55, 84, 85, 86, 87, 103, 110, 56, 57, 58, 59, 88, 89, 90, 91, 102, 109, 60, 61, 62, 63, 92, 93, 94, 95, 101, 108, 64, 65, 66, 67, 96, 97, 98, 99, 100, 0, 28, 29, 30, 31, 32, 33, 34, 35, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+u8 dr_xy[128] = {127, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 99, 91, 92, 93, 94, 95, 96, 97, 98, 11, 12, 13, 14, 21, 22, 23, 24, 31, 32, 33, 34, 41, 42, 43, 44, 51, 52, 53, 54, 61, 62, 63, 64, 71, 72, 73, 74, 81, 82, 83, 84, 15, 16, 17, 18, 25, 26, 27, 28, 35, 36, 37, 38, 45, 46, 47, 48, 55, 56, 57, 58, 65, 66, 67, 68, 75, 76, 77, 78, 85, 86, 87, 88, 89, 79, 69, 59, 49, 39, 29, 19, 80, 70, 60, 50, 40, 30, 20, 10, 1, 2, 3, 4, 5, 6, 7, 8, 0, 0, 0, 0};
+
+void app_surface_event(u8 type, u8 index, u8 value) {
+	if (value != 0) value = 127;
+	
+	if (index == 0) {
+		if (value != 0) {
+			setup = !setup;
+			clear();
+			if (setup) {
+				enterSetup();
+			} else {
+				exitSetup();
+			}
+		}
+	
+	} else if (setup) {
+		if (value != 0) {
+			if (index == 15) {
+				initPalette(novation);
+			}
+			if (index == 16) {
+				initPalette(mat1);
+			}
+			if (index == 17) {
+				loadPalette();
+			}
+			if (index == 18) {
+				savePalette();
+			}
+		}
+	
+	} else {
+		hal_send_midi(USBMIDI, 0x90, xy_dr[index], value);
+	}
+}
+
+void app_midi_event(u8 port, u8 status, u8 d1, u8 d2) {
+    if (port == USBMIDI && !setup) {
+    	switch (status) {
+    		case 0x95: {
+    			led(dr_xy[d1], d2);
+    			break;
+    		}
+    		case 0x85: {
+    			led(dr_xy[d1], 0);
+    			break;
+    		}
+    		case 0xB5: {
+    			edit(d1, d2);
+    			break;
+    		}
+    	}
+    }
+}
+
+/*----------------------------*/
+
+void app_sysex_event(u8 port, u8 * data, u16 count) {}
+void app_aftertouch_event(u8 index, u8 value) {}
+void app_cable_event(u8 type, u8 value) {}
+
+/*----------------------------*/
+
+void app_init(const u16 *adc_raw)
+{
+	loadPalette();
+}
