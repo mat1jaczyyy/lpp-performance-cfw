a999cb9d8eec54f4edb6c2e5690f3eeefb4edd7a
mat1jaczyyy <dominik.matijaca@skole.hr>
2018-08-09 20:15:06 +0200

    Change Programmer mode to channel 1
    Required by FL Studio for communication.

--- app.c
+++ app.c
@@ -1910,12 +1910,12 @@ void programmer_surface_event(u8 p, u8 v, u8 x, u8 y) {
 		if (v != 0) mode_update(mode_setup);
 	
 	} else {
-		hal_send_midi(USBSTANDALONE, (x == 0 || x == 9 || y == 0 || y == 9)? 0xB3 : ((v == 0)? 0x83 : 0x93), p, v);
+		hal_send_midi(USBSTANDALONE, (x == 0 || x == 9 || y == 0 || y == 9)? 0xB0 : ((v == 0)? 0x80 : 0x90), p, v);
 	}
 }
 
 void programmer_midi_event(u8 port, u8 t, u8 ch, u8 p, u8 v) {
-	if (port == USBSTANDALONE && ch == 0x3) {
+	if (port == USBSTANDALONE && ch == 0x0) {
 		if (t == 0x8) {
 			v = 0; // Note off
 			t = 0x9;
