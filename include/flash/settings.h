42329f3b984f15f8d852f18cef1cb2365a3cca07
mat1jaczyyy <mat1jaczyyy@gmail.com>
2018-08-25 22:55:12 +0200

    Add performance_xy_enabled to flash
    Also fixes a small issue where palette_selected doesn't load properly

--- include/flash/settings.h
+++ include/flash/settings.h
@@ -2,7 +2,8 @@
 #define SETTINGS_H
 
 #include "app_defs.h"
+#include "led/palettes.h"
 
-u8 palette_selected, vel_sensitive, top_lights_config;
+u8 palette_selected, vel_sensitive, top_lights_config, performance_xy_enabled;
 
 #endif
