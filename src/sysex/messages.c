041fc47927badbc217a0aabab1905b040cb819a6
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-06-26 15:48:55 +0200

    SysEx: Bump device inquiry version
    Fixes #22.

--- src/sysex/messages.c
+++ src/sysex/messages.c
@@ -4,7 +4,7 @@ const u8 syx_device_inquiry[syx_device_inquiry_length] = {0xF0, 0x7E, 0x7F, 0x06
 const u8 syx_device_inquiry_response[syx_device_inquiry_response_length] = {0xF0, 0x7E,
                                                                       0x00,                                                  // Device ID
                                                                       0x06, 0x02, 0x00, 0x20, 0x29, 0x51, 0x00, 0x00, 0x00,  // Constant
-                                                                      0x00, 0x63, 0x66, 0x77,                                // Firmware rev. (4 bytes) - CFW
+                                                                      0x00, 0x63, 0x66, 0x78,                                // Firmware rev. (4 bytes) - CFX (CFW++)
                                                                       0xF7};
 
 const u8 syx_challenge[syx_challenge_length] = {0xF0, 0x00, 0x20, 0x29, 0x02, 0x10, 0x40}; // Also Live quit message!
